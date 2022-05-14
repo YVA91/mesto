0e67b0c6be7b84c76fe3707cb814d097ad8c483b
YVA91 <Yahnev91@yandex.ru>
2022-05-14 17:09:41 +0300

    Прверочная работа №7 класс Card

--- script/cards.js
+++ script/cards.js
@@ -1,26 +1,62 @@
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
+import { photoBig, figcaption, openPopup, buttonOpenImgPopup } from './script.js'
+
+export class Card {
+  constructor(name, link, template) {
+    this._name = name;
+    this._link = link;
+    this._template = template
+  }
+
+  _getTemplate = () => {
+    const template = document.querySelector('.photo-template').content.querySelector('.photo__item').cloneNode(true);
+    return template;
+  }
+
+  generateCard = () => {
+    this._element = this._getTemplate();
+    this._element.querySelector('.photo__item-img').src = this._link;
+    this._element.querySelector('.photo__item-img').alt = this._name;
+    this._element.querySelector('.photo__item-title').textContent = this._name
+
+    this._buttonDeleteElement = this._element.querySelector('.photo__remove');
+    this._buttonLikeElement = this._element.querySelector('.photo__item-like');
+    this._buttonShowElement = this._element.querySelector('.photo__item-img');
+
+
+
+
+
+    this._setEventListeners();
+    return this._element;
+  }
+
+
+  _setEventListeners = () => {
+    this._buttonDeleteElement.addEventListener('click', () => {
+      this._deleteCard();
+    });
+    this._buttonLikeElement.addEventListener('click', () => {
+      this._likeCard();
+    });
+    this._buttonShowElement.addEventListener('click', () => {
+      this._bigPhotoCard();
+    });
+  };
+  
+
+  _deleteCard = () => {
+    this._buttonDeleteElement.closest('.photo__item').remove();
+  };
+
+  _likeCard = () => {
+    this._buttonLikeElement.classList.toggle('photo__item-like_actve');
+  };
+
+  _bigPhotoCard = () => {
+  photoBig.src = this._link;
+  photoBig.alt = this._name;
+  figcaption.textContent = this._name;
+  openPopup(buttonOpenImgPopup)
+};
+
+}
--- script/script.js
+++ script/script.js
@@ -1,3 +1,33 @@
+
+import { Card } from "./cards.js";
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
 const nameInput = document.querySelector('#name');
 const jobInput = document.querySelector('#job'); 
 const formNewName = document.querySelector('.popup_new-name');
@@ -12,16 +42,16 @@ const photoList = document.querySelector('.photo');
 const photoTemplate = document.querySelector('.photo-template');
 const newTitle = document.querySelector('#title');
 const newLink = document.querySelector('#link');
-const buttonOpenImgPopup = document.querySelector('.popup_open-photo');
-const photoBig = document.querySelector('.popup__item-img');
-const figcaption = document.querySelector('.popup__item-title');
+export const buttonOpenImgPopup = document.querySelector('.popup_open-photo');
+export const photoBig = document.querySelector('.popup__item-img');
+export const figcaption = document.querySelector('.popup__item-title');
 const buttonCloseBigPhoto = document.querySelector('.popup__close_img');
 const formPlaceReset = document.querySelector('#resetformnewplace')
 const formNameReset = document.querySelector('#resetformnewname')
 const popupList = document.querySelectorAll('.popup');
 
 
-function openPopup(item) {
+export function openPopup(item) {
   item.classList.add('popup_opened');
   document.addEventListener('keydown', handleEcsClosePopup)
 };
@@ -45,56 +75,29 @@ function handleSaveNewName(evt) {
   closePopup(formNewName);
 }
 
-function getElementPhoto(name, link) {
-  const templateNewPhoto = photoTemplate.content.cloneNode(true);
-  const photoItemImgAttribute = templateNewPhoto.querySelector('.photo__item-img');
-  templateNewPhoto.querySelector('.photo__item-title').textContent = name;
-  photoItemImgAttribute.src = link;
-  photoItemImgAttribute.alt = name;
-
-  const deleteButton = templateNewPhoto.querySelector('.photo__remove');
-
-  deleteButton.addEventListener('click', removeElement);
-  photoItemImgAttribute.addEventListener('click', handleOpenPhoto);
-  templateNewPhoto.querySelector('.photo__item-like').addEventListener('click', handleLike)
-
-  return templateNewPhoto;
-};
-
-function handleOpenPhoto(element) {
-  photoBig.src = element.target.src;
-  photoBig.alt = element.target.alt;
-  figcaption.textContent = element.target.alt;
-  openPopup(buttonOpenImgPopup)
-};
-
 function handleOpenPopupNewName() {
   nameInput.value = userName.textContent;
   jobInput.value = userJop.textContent;
-  resetValidationForm(config, formNewName);
+  //resetValidationForm(config, formNewName);
   openPopup(formNewName)
 }
 
 function handleOpenPopupNewPlace() {
   formPlaceReset.reset()
-  resetValidationForm(config, formNewPhoto);
+  //resetValidationForm(config, formNewPhoto);
   openPopup(formNewPhoto)
 }
 
-function handleAddElement(evt) {
-  evt.preventDefault();
-  const putPlace = getElementPhoto(newTitle.value, newLink.value);
-  photoList.prepend(putPlace);
-  closePopup(formNewPhoto);
-}
-
-function handleLike(evt) {
-  evt.target.classList.toggle('photo__item-like_actve');
+function createPhoto (item) {
+  const card = new Card(item.name, item.link);
+  const cardElement = card.generateCard();
+  document.querySelector('.photo').append(cardElement);
 };
 
-function removeElement(evt) {
-  const removePhoto = evt.target.closest('.photo__item');
-  removePhoto.remove();
+function handleAddElement(evt) {
+  evt.preventDefault();
+  createPhoto ({ name: newTitle.value, link: newLink.value });
+  closePopup(formNewPhoto);
 }
 
 buttonCloseFormNewPhoto.addEventListener('click', () => closePopup(formNewPhoto));
@@ -107,16 +110,4 @@ buttonNewPlace.addEventListener('click', handleOpenPopupNewPlace);
 formNewName.addEventListener('submit', handleSaveNewName);
 formNewPhoto.addEventListener('submit', handleAddElement);
 
-
-initialCards.forEach(element => {
-  photoList.append(getElementPhoto(element.name, element.link));
-});
-
-popupList.forEach((element) => { 
-  element.addEventListener('click', (e) => {
-      if (e.target.classList.contains('popup_opened')) {
-          closePopup(element);
-      }
-  });
-});
-
+initialCards.forEach(createPhoto);
